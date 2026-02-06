0463fd16ec02680b7096b588df5ee3cc09e7d03f
hbz9 <49538764+hbz9@users.noreply.github.com>
2026-02-06 11:34:55 +0100

    Delete schema/quality directory

--- schema/quality/01_missing.sql
+++
@@ -1,8 +0,0 @@
--- Missing values checks
-SELECT COUNT(*) AS missing_population
-FROM population
-WHERE population IS NULL;
-
-SELECT COUNT(*) AS missing_region_name
-FROM region
-WHERE region_name IS NULL OR region_name = '';
